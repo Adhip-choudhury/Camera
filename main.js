96ab763da49e55671de386f1825652a71a08ebfb
Adhip-choudhury <79558085+Adhip-choudhury@users.noreply.github.com>
2021-06-17 20:50:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,14 +12,25 @@ Recognition.onresult=function (event){
     content= event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML=content;
     console.log(content);
-    speak();
+    if(content == "take my selfie"){
+        console.log("taking selfie...");
+        speak();
+    }
+    
 }
 function speak(){
     var synth=window.speechSynthesis;
-    var speak_data=content;
+    var speak_data="Taking your selfie in 5 second";
     var say_this=new SpeechSynthesisUtterance(speak_data);
     synth.speak(say_this);
     Webcam.attach(camera);
+    setTimeout(function ()
+    {
+    take_snapshot();
+    save();
+    }, 5000);
+
+
 }
 
 Webcam.set({
@@ -30,3 +41,16 @@ Webcam.set({
 });
 
 var camera=document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function (data_url){
+     document.getElementById("click_picture").innerHTML="<img id='selfie' src='"+data_url+"'>";
+    });
+}
+
+function save(){
+    var Link=document.getElementById("Link");
+    var img=document.getElementById("selfie").src;
+    Link.href=img;
+    Link.click();
+}
